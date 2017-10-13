2c688694cd9b0e7a95b5a8f2437f93fb48a21862
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-13 10:20:47 -0300

    das

--- tragetory/extrator3.py
+++ tragetory/extrator3.py
@@ -10,10 +10,10 @@ from ikpy import plot_utils
 
 
 #CONFIGS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-deslocamentoZpes = 2.5
+deslocamentoZpes = 3.
 deslocamentoXpes = 5.
 deslocamentoYpes = 0.
-deslocamentoYpelves = 3.
+deslocamentoYpelves = 2.5
 periodo = 20.
 nEstados = 125
 dMovx = deslocamentoXpes/nEstados
