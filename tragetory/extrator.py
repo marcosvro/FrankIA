e408f2954517e40031c529f544e8b82961cb0004
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-13 10:45:11 -0300

    ads

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -12,7 +12,7 @@ import serial
 #CONFIGS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 deslocamentoZpes = 2.
 deslocamentoXpes = 7.
-deslocamentoYpelves = 4.
+deslocamentoYpelves = 3.6
 periodo = 0.25
 nEstados = 200
 dMovx = deslocamentoXpes/nEstados
