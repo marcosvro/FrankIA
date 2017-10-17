2f86ce30e1b4b2a068993b6413e1dd340c609a12
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-17 18:20:22 -0200

    ds

--- tragetory/extrator3.py
+++ tragetory/extrator3.py
@@ -12,14 +12,14 @@ from ikpy import plot_utils
 
 
 #CONFIGS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-deslocamentoZpes = 1.5
-deslocamentoXpes = 3.
+deslocamentoZpes = 0
+deslocamentoXpes = 0.
 deslocamentoYpes = 0.
-deslocamentoYpelves = 1.9
+deslocamentoYpelves = 1.5
 
 
 periodo = 20.
-nEstados = 125
+nEstados = 75
 dMovx = deslocamentoXpes/nEstados
 frameRate = periodo/nEstados
 data_foot = np.zeros((nEstados,8), dtype=np.uint8)
@@ -80,7 +80,7 @@ jointsp2f = ik2;
 	parm: indice(int) - diz em qual posicao do vetor de tragetoria deve ser armazenada a cinematica e qual momento da tragetoria calcular'''
 def thread_cinematica_pelves(indice):
 	pos = pos_inicial_pelves
-	p = (deslocamentoXpes/2)*((np.exp((2*(indice-nEstados/2))/50) - np.exp((2*(indice-nEstados/2))/-50))/(np.exp((2*(indice-nEstados/2))/50)+np.exp((2*(indice-nEstados/2))/-50)))
+	p = (deslocamentoXpes/2)*((np.exp((2*(indice-nEstados/2))/25) - np.exp((2*(indice-nEstados/2))/-25))/(np.exp((2*(indice-nEstados/2))/25)+np.exp((2*(indice-nEstados/2))/-25)))
 	pos[0] = 0.5*p + 4.33
 	pos[1] = -deslocamentoYpelves*np.sin(indice*np.pi/nEstados)
 	frame_target = np.eye(4)
@@ -125,8 +125,8 @@ def thread_cinematica_pelves(indice):
 	parm: indice(int) - diz em qual posicao do vetor de tragetoria deve ser armazenada a cinematica e qual momento da tragetoria calcular'''
 def thread_cinematica_pe(indice):
 	pos = pos_inicial_pe
-	pos[0] = 4.33 + 0.5*(-deslocamentoXpes/2)*((np.exp((2*(indice-nEstados/2))/50) - np.exp((2*(indice-nEstados/2))/-50))/(np.exp((2*(indice-nEstados/2))/50)+np.exp((2*(indice-nEstados/2))/-50)))
-	pos[2] = 14. - deslocamentoZpes*np.exp(-((indice-nEstados/2)**2)/600)
+	pos[0] = 4.33 + 0.5*(-deslocamentoXpes/2)*((np.exp((2*(indice-nEstados/2))/25) - np.exp((2*(indice-nEstados/2))/-25))/(np.exp((2*(indice-nEstados/2))/25)+np.exp((2*(indice-nEstados/2))/-25)))
+	pos[2] = 14. - deslocamentoZpes*np.exp(-((indice-nEstados/2)**2)/200)
 
 	frame_target = np.eye(4)
 	frame_target[:3, 3] = pos
