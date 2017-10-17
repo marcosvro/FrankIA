3abbe34db4a63ae1a1e6d7a62ae47b27e250040e
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-17 19:24:08 -0200

    ds

--- tragetory/extrator3.py
+++ tragetory/extrator3.py
@@ -13,7 +13,7 @@ from ikpy import plot_utils
 
 #CONFIGS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 deslocamentoZpes = 1.5
-deslocamentoXpes = 0.
+deslocamentoXpes = 2.
 deslocamentoYpes = 0.
 deslocamentoYpelves = 1.9
 
