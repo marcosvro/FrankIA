581199858cbcf41a7d976bbce5616c23588f2852
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-24 17:35:34 -0200

    das

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -36,8 +36,8 @@ ser = serial.Serial('/dev/ttyUSB0', 230400, timeout=0)
 ser_uno = serial.Serial('/dev/ttyUSB1', 230400, timeout=0)
 
 #camera process inicialize
-#os.system("python ../visao/visao3.py&")
-cam_proc = int(check_output(["pidof","python"]))
+os.system("python ../visao/visao3.py&")
+cam_proc = map(int,check_output(["pidof", "python"]).split())
 
 print (cam_proc, type(cam_proc))
 exit()
