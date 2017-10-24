3383fd51cd7f28f7ea36864b84dec41ebb1768a7
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-24 21:45:17 -0200

    dsa

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -319,8 +319,12 @@ try:
 				else:
 					if math.fabs(rota_esq) == 1:
 						rota_esq = 0
+					elif math.fabs(rota_esq) == 2:
+						rota_esq /= 2					
 					if math.fabs(rota_dir) == 1:
 						rota_dir = 0
+					elif math.fabs(rota_dir) == 2:
+						rota_dir /= 2
 				
 			state = (state+1)%nEstados
 
