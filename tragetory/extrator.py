20c736c25bde8702d2ebf44c2493e0b9fe06fbd2
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-10-28 17:25:29 -0200

    sda

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -403,7 +403,7 @@ try:
 			send_test = np.array([255]+pelv_iner+data_pelv[state].tolist()+[254], dtype=np.uint8)
 			#ser.write(bytes(send_test))
 		#print (state, " --- ", send_test)
-		teste_porra = bytes(send_test[:9]+[254])
+		teste_porra = str(bytes(send_test[:9]+[254]))
 		ser.write(teste_porra)
 		print (teste_porra)
 
