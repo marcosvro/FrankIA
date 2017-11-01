d36742617ec23f1ecf87bd8638238ceda7564d94
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-11-01 14:42:55 -0200

    dsa

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -349,7 +349,7 @@ try:
 			pos_atual = np.array(np.rint(qua2), dtype=np.int)
 			print (pos_atual)
 		elif len(qua2) != 0:
-			print (buff2)
+			#print (buff2)
 		ser.flushInput()
 		#Inersial read (100hz)
 		buff = ser_uno.readline()
