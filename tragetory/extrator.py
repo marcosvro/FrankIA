04fd82d13c6d49313cf0eb618c64c9b067e1fddd
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-11-04 04:36:34 -0200

    controle

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -322,10 +322,10 @@ try:
 			send_pelv = np.array([255]+data_pelv[state].tolist()+[254], dtype=np.uint8)
 			send_test = np.array([255]+data_foot[state].tolist()+[254], dtype=np.uint8)
 			#print (send_test, send_pelv)
-			ser.write(struct.pack('>10B', *(send_test.tolist())))
-			ser2.write(struct.pack('>10B', *(send_pelv.tolist())))
-			#ser.write(struct.pack('>10B', 255, 90, 90, 90, 90, 90, 90, 90, 90, 254))
-			#ser2.write(struct.pack('>10B', 255, 90, 90, 90, 90, 90, 90, 90, 90, 254))		
+			#ser.write(struct.pack('>10B', *(send_test.tolist())))
+			#ser2.write(struct.pack('>10B', *(send_pelv.tolist())))
+			ser.write(struct.pack('>10B', 255, 90, 90, 90, 90, 90, 90, 90, 90, 254))
+			ser2.write(struct.pack('>10B', 255, 90, 90, 90, 90, 90, 90, 90, 90, 254))		
 		else:
 			'''
 			if rota_dir == 1:
