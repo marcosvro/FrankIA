384d4549b081a2e4b0169998b36ff1dbdb59572a
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-11-04 00:38:50 -0200

    lucas

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -35,9 +35,9 @@ ab6 = np.array([[0.,0.,1.,0.],[0.,1.,0.,-4.5],[-1.,0.,0.,-22.99],[0.,0.,0.,1.]],
 
 #COMUNICATION +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #serial
-ser = serial.Serial('/dev/ttyUSB1', 230400, timeout=0)
+ser = serial.Serial('/dev/ttyUSB0', 230400, timeout=0)
 ser2 = serial.Serial('/dev/ttyUSB2', 230400, timeout=0)
-ser_uno = serial.Serial('/dev/ttyUSB0', 230400, timeout=0)
+ser_uno = serial.Serial('/dev/ttyUSB1', 230400, timeout=0)
 
 #camera process inicialize
 os.system("python ../visao/visao3.py&")
