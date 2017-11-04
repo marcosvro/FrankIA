302b112edb73791f295de73352bca9a598e2b5c2
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-11-04 12:41:11 -0200

    controle

--- tragetory/extrator.py
+++ tragetory/extrator.py
@@ -327,8 +327,8 @@ try:
 			'''
 			#print (data_foot[state][5], " -- vire ", rot_desvio, " graus")
 			#pelv_iner = np.array([255]+data_pelv[state][:3].tolist()+iner[:2].tolist()+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
-			send_pelv = np.array([255]+data_pelv[state].tolist()+[254], dtype=np.uint8)
-			#send_pelv = np.array([255]+pos_controle[:5]+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
+			#send_pelv = np.array([255]+data_pelv[state].tolist()+[254], dtype=np.uint8)
+			send_pelv = np.array([255]+pos_controle[:5]+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
 			send_test = np.array([255]+data_foot[state].tolist()+[254], dtype=np.uint8)
 			#print (send_test, send_pelv)
 			ser.write(struct.pack('>10B', *(send_test.tolist())))
@@ -353,8 +353,8 @@ try:
 			'''
 			#print (data_pelv[state][5], " -- vire ", rot_desvio, " graus")
 			#pelv_iner = np.array([255]+data_pelv[state][:3].tolist()+iner[:2].tolist()+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
-			send_pelv = np.array([255]+data_pelv[state].tolist()+[254], dtype=np.uint8)
-			#send_pelv = np.array([255]+pos_controle[:5]+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
+			#send_pelv = np.array([255]+data_pelv[state].tolist()+[254], dtype=np.uint8)
+			send_pelv = np.array([255]+pos_controle[:5]+data_pelv[state][5:].tolist()+[254], dtype=np.uint8)
 			send_test = np.array([255]+data_foot[state].tolist()+[254], dtype=np.uint8)
 			#print (send_pelv, send_test)
 			ser.write(struct.pack('>10B', *(send_pelv.tolist())))
