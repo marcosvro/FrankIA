38548c21a008ee405414bceca50dba3ab79b22f6
marcosvro <marcos.v.rodrigues@hotmail.com>
2017-11-05 00:47:12 -0200

    controle

--- tragetory/extrator3.py
+++ tragetory/extrator3.py
@@ -11,7 +11,7 @@ from ikpy import plot_utils
 
 
 #CONFIGS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-deslocamentoZpes = 1.5
+deslocamentoZpes = 1.
 deslocamentoXpes = 0.
 deslocamentoYpes = 0.
 deslocamentoYpelves = 1.2
