9c78b422f831a60a728940cd3a00875c4aafe7c0
jordy <jordyfaria0@gmail.com>
2017-11-07 00:34:38 -0200

    teste de led

---
+++ Teste/testeLED.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+
+LED = 21		# Pino do LED
+PIN_IN = 20     # Pino do switch
+
+# Configuração inicial
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(PIN_IN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED,GPIO.LOW)
+
+while True:
+    if GPIO.input(PIN_IN):
+        GPIO.output(LED,GPIO.HIGH)
+    else:
+        GPIO.output(LED,GPIO.LOW)
